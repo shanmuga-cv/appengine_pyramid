4c39ade21115edd4f1d129f06ce123bbed392a88
shanmuga <shanmuga.chidambaravel@gmail.com>
2017-11-14 05:52:13 +0530

    autocomplete from search api

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
+# coding: utf-8
+
+from google.appengine.api import search
 from flask import Flask, render_template, request, jsonify
+
+idx = search.Index(name='products_idx')
 app = Flask("sample autocomplete")
-
-words = ['Agent', 'Android', 'Anniversary', 'But', 'Chrome', 'Creation',
-    'Creators', 'Developer', 'Edge', 'Emulation', 'F12', 'Firefox',
-    'Fortunately', 'Google', 'Here', 'However', 'ISO', 'If', 'Language',
-    'Media', 'Microsoft', 'Mozilla', 'N', 'PC', 'Press', 'Single', 'So',
-    'Switcher', 'That', 'Tool', 'Tools', 'Update', 'User', 'Windows', 'With',
-    'You', 'a', 'able', 'access', 'accessing', 'actually', 'agent', 'also',
-    'an', 'and', 'are', 'around', 'as', 'available', 'avoid', 'be', 'bit',
-    'both', 'browser', 'bummer', 'but', 'called', 'can', 'change', 'click',
-    'created', 'decided', 'dedicated', 'device', 'different', 'do', 'don',
-    'download', 'easily', 'easy', 'enough', 'extension', 'extensions',
-    'files', 'fly', 'folks', 'for', 'from', 'get', 'giant', 'hands', 'has',
-    'have', 'how', 'identifies', 'if', 'in', 'is', 'it', 'landing', 'latest',
-    'lets', 'link', 'messing', 'now', 'of', 'on', 'operating', 'page',
-    'provides', 'reads', 'redirect', 'redirected', 'released', 's', 'say',
-    'sees', 'shouldn', 'similar', 'so', 'software', 'still', 'straight',
-    'strangely', 'system', 't', 'tab', 'that', 'the', 'there', 'therefore',
-    'to', 'too', 'use', 'user', 'users', 'version', 'want', 'website',
-    'well', 'which', 'who', 'will', 'with', 'without', 'you', 'your']
-
-
 
 @app.route('/')
 def form():
@@ -29,7 +13,11 @@ def form():
 @app.route('/autocomplete')
 def query_autocomplete():
     q = request.args['term']
-    print(q)
-    q = q and q.lower()
-    results = [w for w in words if q in w.lower()]
-    return jsonify(results)
+    if not q:
+        return jsonify([])
+    q = q.lower()
+    results = idx.search('product_name: \"{}\"'.format(q))
+    resuls_dict = [{f.name: f.value for f in r.fields} for r in results]
+    return jsonify([r['product_name'] for r in resuls_dict])
+
+
