2c9aaec16298591821b0922798fb56b152cdd1a5
shanmuga <shanmuga.chidambaravel@gmail.com>
2017-11-14 08:39:37 +0530

    background thread failed attempt

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ def query_autocomplete():
     return jsonify([r['product_name'] for r in resuls_dict])
 
 
-#####
+##### doesn't work. app engine doesnot allow background thread after request is processed.
+# google appengine backend or task queue needs to be implements
 from google.cloud import datastore
-from itertools import islice
-import json
 datastore_client = datastore.Client()
-datastore_results = [x for x in islice(datastore_client.query(kind='products').fetch(), 0, 100)]
+
 def datastore_to_search(dr):
     fields = []
     if dr['product_name']:
@@ -39,9 +38,47 @@ def datastore_to_search(dr):
         fields.append(search.AtomField(name='price_symbol', value=dr['price_symbol']))
     return search.Document(doc_id=str(dr.key.id), fields=fields)
 
-for dr in datastore_results:
+def insert(dr):
     idx.put(datastore_to_search(dr))
 
-print('inserted')
+from threading import Thread, current_thread
+from time import sleep
+
+
+job_generator = iter(datastore_client.query(kind='products').fetch())
+
+counter = 0
+def worke_func():
+    global counter
+    global job_generator
+    try:
+        while True:
+            dr = job_generator.next()
+            print(current_thread().name, dr)
+            counter += 1
+            sleep(3)
+    except StopIteration as e:
+        pass
+    except Exception as e:
+        raise e
+
+
+workers = [Thread(name='worker_'+str(i), target=worke_func) for i in range(5)]
+[w.start() for w in workers]
 
 # in case of ChunkedEncodingError apply patch https://stackoverflow.com/a/37181539
+
+@app.route('/counter')
+def get_counter():
+    global counter
+    return "total records inserted: {}".format(counter)
+
+@app.route('/workers')
+def worers_status():
+    global workers
+    return jsonify([w.is_alive() for w in workers])
+
+@app.route('/start_workers')
+def start_workers():
+    global workers
+    return jsonify([w.start() for w in workers])
